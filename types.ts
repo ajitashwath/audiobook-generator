7a7f29b7c4a96ad144855e2c3a225fa735c6de56
{Ajit <ajitashwathr23@lpu.in>
2025-01-01 10:32:35 +0530

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,65 @@
+[33mcommit dd086d36d241d9193863606e3e327e71f4b78a32[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: {Ajit <ajitashwathr23@lpu.in>
+Date:   Mon Dec 30 13:10:19 2024 +0530
+
+    Update page.tsx
+
+[33mcommit 19c786016b3383217a01f4d924ac3afdfb4f8df6[m
+Author: {Ajit <ajitashwathr23@lpu.in>
+Date:   Mon Dec 30 13:10:16 2024 +0530
+
+    Create types.ts
+
+[33mcommit 1de90ed5fba35fc2f4e028d608e7c9621f22bcb0[m
+Author: {Ajit <ajitashwathr23@lpu.in>
+Date:   Mon Dec 30 12:57:35 2024 +0530
+
+    Update layout.tsx
+
+[33mcommit a4c6fa934449c37c52d54b8cbc785300d4da9322[m
+Author: {Ajit <ajitashwathr23@lpu.in>
+Date:   Sun Dec 29 13:56:05 2024 +0530
+
+    Update layout.tsx
+
+[33mcommit 847d155d39976c3303f10401ba3c3b3fb2aa8435[m
+Author: {Ajit <ajitashwathr23@lpu.in>
+Date:   Sun Dec 29 11:51:20 2024 +0530
+
+    Modified files
+
+[33mcommit 555c93f5bed1b09bc2a30939642ba4a84127234f[m
+Author: {Ajit <ajitashwathr23@lpu.in>
+Date:   Sat Dec 28 14:54:11 2024 +0530
+
+    Create route.ts
+
+[33mcommit 698b1f3996f0adc35dbb3ad36bb6259fcd767512[m
+Author: {Ajit <ajitashwathr23@lpu.in>
+Date:   Sat Dec 28 14:54:08 2024 +0530
+
+    Create FileUpload.tsx
+
+[33mcommit 3705a8d171eb27ef1ad51a49ee2b52e64f689afb[m
+Author: {Ajit <ajitashwathr23@lpu.in>
+Date:   Sat Dec 28 14:54:05 2024 +0530
+
+    Update layout.tsx
+
+[33mcommit 771db0a204d0cce61829b7726e5d2c511dc39d26[m
+Author: {Ajit <ajitashwathr23@lpu.in>
+Date:   Sat Dec 28 14:54:02 2024 +0530
+
+    Update page.tsx
+
+[33mcommit 77212cc931136046da62c9a82268d0b6a5fcb0c6[m
+Author: {Ajit <ajitashwathr23@lpu.in>
+Date:   Sat Dec 28 13:50:14 2024 +0530
+
+    a
+
+[33mcommit f2e443ea02796e8902113766bfd34eeeb05ca4fe[m
+Author: {Ajit <ajitashwathr23@lpu.in>
+Date:   Mon Dec 23 13:28:30 2024 +0530
+
+    Initial commit from Create Next App
