c73b0062a05999d4675918b75c505f57a4ed1d4a
{Ajit <ajitashwathr23@lpu.in>
2025-01-04 13:20:29 +0530

    Update types.ts

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,15 +1,3 @@
-export type UploadResponse = {
-    message: string;
-    data?: {
-        fileName: string;
-        fileSize: number;
-        fileType: string;
-        uploadedAt: string;
-        chapters?: Chapter[];
-    };
-    error?: string;
-};
-
 export type Chapter = {
     id: string;
     title: string;
@@ -17,13 +5,20 @@ export type Chapter = {
     audioUrl?: string;
     duration?: number;
     pageStart?: number;
-    pageENd?: number;
+    pageEnd?: number;
 };
-
-export type FileStatus = 'idle' | 'uploading' | 'processing' | 'completed' | 'error';
-export type FileMetaData = {
-    name: string;
-    size: number;
-    type: string;
-    lastModified: number;
+  
+  export type UploadResponse = {
+    message: string;
+    success?: boolean;
+    data?: {
+      fileName?: string;
+      fileSize?: number;
+      fileType?: string;
+      uploadedAt?: string;
+      chapters?: Chapter[];
+    };
+    error?: string;
 };
+  
+  export type FileStatus = 'idle' | 'uploading' | 'processing' | 'completed' | 'error';
